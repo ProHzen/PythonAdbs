6c921d66b93d2c1d2cb737a66f217df088c5ce9a
yangsz <369013520@qq.com>
2017-09-03 13:54:21 +0800

    第二波上传

--- androidTools.py
+++ androidTools.py
@@ -2,11 +2,12 @@ import subprocess
 import re
 import os
 import os.path
-
+import time
 
 def sh(command):
     p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     return p.stdout.read()
+
 
 def ensure_dir(f):
     try:
@@ -17,16 +18,23 @@ def ensure_dir(f):
         return False
     return True
 
-def getCurrrentActivity():
 
+def getCurrrentActivity():
     # 采用正则表达式截取
-    result = sh('adb shell "dumpsys activity | grep mFocusedActivity"').decode("utf-8")
+    result = sh('adb shell "dumpsys activity | grep mFocusedActivity"').decode("utf-8").lstrip()
     regularly = r'[\s\/{1}\s]'
+    print(result)
     result = re.split(regularly, result)
-    packageName = result[6]
-    className = result[7]
+    print(result)
+    for index in range(len(result)):
+        if (result[index].find('com') == 0):
+            packageIndex = index
+            break
+    packageName = result[packageIndex]
+    className = result[packageIndex + 1]
     print("packageName = " + packageName + " className = " + className)
     str = (packageName, className)
+
     # 不采用正则表达式截取
     # index = str1.find('com')
     # str2 = str1[index:].split('/')
@@ -37,25 +45,67 @@ def getCurrrentActivity():
 
     return str
 
-activity = getCurrrentActivity()
-packageName = activity[0]
-print(packageName)
 
-path = sh('adb shell pm path '+ packageName).decode("utf-8")
-s = r'\:'
-path1 = re.split(s, path)
+def getCurrentApk():
+    activity = getCurrrentActivity()
+    packageName = activity[0]
+    print(packageName)
+    getApkByPackageName(packageName)
 
 
-# result = sh(' adb pull ' + path1[1] + ' ~/apks')
-ensure_dir(os.path.join(os.path.abspath('.'), 'apks'))
-p = subprocess.Popen(' adb pull ' + path1[1] + ' ~/apks', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+def getApkByPackageName(packageName):
+    path = sh('adb shell pm path ' + packageName).decode("utf-8").rstrip()
+    s = r'\:'
+    path1 = re.split(s, path)
+    # result = sh(' adb pull ' + path1[1] + ' ~/apks')
+    ensure_dir(os.path.join(os.path.abspath('.'), 'apks'))
+    p = subprocess.Popen(' adb pull ' + path1[1] + " " + os.path.join(os.path.abspath('.'), 'apks'), shell=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while (True):
+        line = p.stdout.readline().decode("utf-8")
+        if not line:
+            break
+        print(line)
 
-# while(True):
-#     line = p.stdout.readline().decode("utf-8")
-#     if not line:
-#         break
-#     print(line)
 
-print(path1[1])
-# print(os.path.abspath('.'))
-# print(result)
+def getScreenShots():
+    ensure_dir(os.path.join(os.path.abspath('.'), 'images'))
+    timeName = time.strftime("%Y%m%d%H%M%S", time.localtime()) + '.png';
+    print(timeName)
+    print('adb exec-out screencap -p > ' + " " + os.path.join(os.path.abspath('.'), 'images', timeName + '.png'))
+    print('adb exec-out screencap -p   /sdcard/' + timeName)
+    print('adb pull /sdcard/' + timeName + '  ' + os.path.join(os.path.abspath('.'), 'images'))
+    p = subprocess.Popen('adb exec-out screencap -p /sdcard/' + timeName,
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p.wait()
+    sh('adb pull /sdcard/' + timeName + '  ' + os.path.join(os.path.abspath('.'), 'images'))
+    sh('adb shell rm /sdcard/' + timeName)
+
+    # while (True):
+    #     line = p.stdout.readline().decode("utf-8")
+    #     if not line:
+    #         break
+    #     print(line)
+
+
+def reboot():
+    sh('adb reboot')
+
+
+def killCurrentApp():
+    activity = getCurrrentActivity()
+    packageName = activity[0]
+    sh('adb shell am force-stop ' + packageName)
+
+
+def clearData():
+    activity = getCurrrentActivity()
+    packageName = activity[0]
+    sh('adb shell pm clear ' + packageName)
+
+def uninstall():
+    activity = getCurrrentActivity()
+    packageName = activity[0]
+    sh('adb uninstall ' + packageName)
+
+uninstall()
